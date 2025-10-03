f9533d305bdd191170861bc85740ec91a3cbc0a2
Mason Andersen <masonandersen3002@gmail.com>
2025-10-02 20:01:18 -0600

    Super basic highlighting feature and phone number parsing with regex

--- Main/AI_Parser/PDF_Parser/spacyTest.py
+++ Main/AI_Parser/PDF_Parser/spacyTest.py
@@ -1,14 +1,52 @@
 import spacy
 from spacypdfreader.spacypdfreader import pdf_reader
-
+import pymupdf
+import re
 # set up a natural language processing model
 nlp = spacy.load('en_core_web_sm')
 
 # read the pdf into a usable format with given packages
 doc = pdf_reader('Addresses.pdf', nlp)
-
+doc2 = pymupdf.open('Addresses.pdf')
 # just print out the recognized entities (address, etc)
 for ent in doc.ents:
     print(f"Entity: {ent.text}, Label: {ent.label_}")
 
+# open input PDF
+# load desired page (0-based page number)
+page = doc2[0]
+
+# gets rectangles (coordinates on pdf) containing a phone number by matching with regex
+# returns the list of those rectangles
+def getPhoneNumberRects(page):
+
+    # regex that matches phone numbers
+    pattern = re.compile(r"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$")
+
+    # get text from the page then find matches with regex
+    words = page.get_text("words")  # extract words on page
+    matches = [w for w in words if pattern.search(w[4])]
+    # list for output
+    rects = []
+    for value in matches:
+        # the way it is read this gives you the coordinates of the phone number matched in the regex above
+        phoneNumberRect = value[0:4]
+        rects.append(phoneNumberRect)
+    return rects
+
+# highlights identified rectangles with a given color as annotations
+# param rects the rectangles list to highlight all
+# param color the color in pymupdf.pdfcolor["whatever"]
+# no return
+def highlightRects(rects, color):
+    for i, rect in enumerate(rects):
+        annot = page.add_highlight_annot(rects)
+        annot.set_colors(stroke=color)
+        annot.update()
+
+rects = getPhoneNumberRects(page)
+highlightRects(rects, pymupdf.pdfcolor["red"])
+
+# save the document with these changes, apply annotations
+doc2.save("output.pdf")
 
